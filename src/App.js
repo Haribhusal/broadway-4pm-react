a84b6c7b7337ea28cdb5ef114f2782c3ef33590c
Haribhusal <bhusalhari89@gmail.com>
2022-08-07 17:12:52 +0545

    initial git commit, just installed react, created header, and carousel component

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import MyHeader from './components/header'
+import CarouselComponent from './components/carousel';
+
+function App() {
+  return (
+    <>
+      <MyHeader />
+      <CarouselComponent />
+    </>
+  );
+}
+
+export default App;
